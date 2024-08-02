1669a31ac38c84f9d44d544ad11e655841aa2708
mfjaconis <matheusjaconis@gmail.com>
2024-08-02 19:42:05 -0300

    criando o botão e seus estilos com props

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,15 @@
+import { Button } from "../../components/button";
 import { Logo } from "../../components/logo";
 import { Header } from "./styles";
 
 export function Home() {
 	return (
 		<Header>
-			<Logo/>
+			<Logo />
+			<div>
+				<Button>Nova transação</Button>
+				<Button>Nova transação</Button>
+			</div>
 		</Header>
 	);
 }
